132b9a7342ca761e1daf1a34fcc19640d30e2de3
Israel Ogunsola <110337578+Issybobo@users.noreply.github.com>
2024-04-01 17:18:20 +0100

    completed the user contoller

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 
 
 const app = express();
@@ -13,8 +15,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); //To parse the incoming request with JSON payloads (from req.bosy controller)
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 
 app.get("/", (req, res) => {
     res.send("Hello world")
